337ed820db30e5ef44b3fb9b60856a6d2b1fcd98
nagelpat <patrik.nagel@gmail.com>
2017-03-31 10:20:46 +0100

    Update generate-settings.sh

--- tasks/generate-settings.sh
+++ tasks/generate-settings.sh
@@ -16,38 +16,7 @@ cat > ${HOME}/.m2/settings.xml <<EOF
       xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                           https://maven.apache.org/xsd/settings-1.0.0.xsd">
 
-      <mirrors>
-          <mirror>
-            <id>${M2_SETTINGS_REPO_ID}</id>
-            <mirrorOf>*</mirrorOf>
-            <url>${M2_SETTINGS_REPO_RELEASE_URI}</url>
-            <name>Artifactory</name>
-          </mirror>
-      </mirrors>
-       <servers>
-         <server>
-           <id>${M2_SETTINGS_REPO_ID}</id>
-           <username>${M2_SETTINGS_REPO_USERNAME}</username>
-           <password>${M2_SETTINGS_REPO_PASSWORD}</password>
-         </server>
-       </servers>
 
-       <profiles>
-         <profile>
-           <id>artifactory</id>
-           <repositories>
-               <repository>
-                   <id>${M2_SETTINGS_REPO_ID}</id>
-                   <name>libs-release</name>
-                   <url>${M2_SETTINGS_REPO_RELEASE_URI}</url>
-               </repository>
-           </repositories>
-         </profile>
-       </profiles>
-
-       <activeProfiles>
-         <activeProfile>artifactory</activeProfile>
-       </activeProfiles>
 </settings>
 
 EOF
